be9b423566a7c4132bf52a0c1ca55ae4cc0e4276
Rio McNeil <102774384+DefinitlyNotRio@users.noreply.github.com>
2022-04-13 00:54:56 +0000

    AHHHH Y INPUT WORK NOT IS WHY!?!?

--- main.cpp
+++ main.cpp
@@ -30,28 +30,53 @@ int main() {
   int questionCount;
   line >> questionCount;
 
+  bool Answered = false;
   string answer;
   string question;
+  int input;
   int questionLine = 7+1;
   int questionCountLine = 7;
   int printAnswers = 0;
   int answerCount;
   int answerCountLine = questionCountLine+2;
-  int answerLines = 0; 
+  int answerLines = 0;
+  int correctAnswer;
+  int correct = 0;
   
   for (int a = 1; a < questionCount; a++) {
-  Gotaline(line, questionLine);
-  line >> question;
-  std::cout << "\n" << question;
-  Gotaline(line, answerCountLine);
-  line >> answerCount;
-    while (printAnswers < answerCount) {
-      Gotaline(line, answerCountLine+1);
-      line >> answer;
-      std::cout << "\n" << answer;
-      answerCountLine++;
-      printAnswers++;
+    Gotaline(line, questionLine);
+    line >> question;
+    std::cout << "\n" << question;
+    
+    Gotaline(line, answerCountLine);
+    line >> answerCount;
+      while (printAnswers < answerCount) {
+        Gotaline(line, answerCountLine+1);
+        line >> answer;
+        std::cout << "\n" << answer;
+        answerCountLine++;
+        printAnswers++;
+      }
+    
+    while (Answered == false) {
+    std::cin >> input;
+    if (std::cin) {
+      if (input == correctAnswer) {
+        correct = correct+1;
+        Answered = true;
+      }
+      else{
+        std::cout << "Sorry the correct answer was " << correctAnswer;
+        Answered = true;
+      }
+    } 
+      if (!std::cin) {
+        std::cout << "Your input must be an integer\n";
+      }
+    cin.clear();
+    cin.ignore();
     }
   }
+  std::cout << "Congratulations you got " << correct << "/" << questionCount;
   return 0;
 }
